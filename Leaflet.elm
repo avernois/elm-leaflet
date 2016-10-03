c60cf701e097d4e8cf49f8dd1c39fdc6f9181d20
Pntbr <pntbr@scopyleft.fr>
2016-10-03 13:09:59 +0200

    create a static html town's list from Elm

--- Leaflet.elm
+++ Leaflet.elm
@@ -1,5 +1,11 @@
 port module Leaflet exposing (..)
-import Html exposing (text)
+import Html exposing (text, ul, li)
+import Html.Attributes exposing (id)
 
 main =
-  text "Hello, Elm"
+  ul [id "towns"] 
+    [ li [] [text "Montpellier"]
+    , li [] [text "Toulouse"]
+    , li [] [text "Lyon"]
+    , li [] [text "Bordeaux"]
+    ]
