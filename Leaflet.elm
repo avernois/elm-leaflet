ec88eec6059814f5a3e192ab207074f9159ebd43
Pntbr <pntbr@scopyleft.fr>
2016-10-03 23:51:54 +0200

    place markers at map initialisation

--- Leaflet.elm
+++ Leaflet.elm
@@ -3,6 +3,7 @@ import Html exposing (Html, text, ul, li)
 import Html.App exposing (program)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (id, attribute)
+import LeafletData exposing (..)
 
 
 
@@ -21,31 +22,20 @@ main = program
 -- Init
 
 
-type alias Town =
-  { name : String
-  , location : { lat : Float, lng : Float }
-  }
-towns : List Town
-towns =
-  [ Town "Montpellier" {lat = 43.610769, lng = 3.876716}
-  , Town "Bordeaux" {lat = 44.837789, lng = -0.579180}
-  , Town "Toulouse" {lat = 43.604652, lng = 1.444209}
-  , Town "Cholet" {lat = 47.059407, lng = -0.879787}
-  , Town "Marseille" {lat = 43.296482, lng = 5.369780}
-  , Town "Lyon" {lat = 47.215586, lng = -1.541820 }
-  ]
-
-
 type alias Model = Town
 
-defaultTown : Town
-defaultTown =
-  Town "Montpellier" {lat = 43.610769, lng = 3.876716}
 
 init : (Model, Cmd Msg)
-init = 
-  ( defaultTown, portActiveTown defaultTown )
-
+init =
+  let
+    defaultTown = LeafletData.defaultTown
+  in
+    ( defaultTown
+    , Cmd.batch
+      [ portActiveTown defaultTown
+      , portPlaces places
+      ]
+    )
 
 
 
@@ -53,6 +43,7 @@ init =
 
 
 port portActiveTown : Town -> Cmd msg
+port portPlaces : List Place -> Cmd msg
 
 
 type Msg
